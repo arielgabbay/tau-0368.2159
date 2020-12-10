35ae633f0c78462254a64856fc282226591d6429
Ariel <arielgabbay@gmail.com>
2020-12-11 00:07:03 +0200

    palindrome.asm updated

--- hw06/hw06_6/palindrome.asm
+++ hw06/hw06_6/palindrome.asm
@@ -119,10 +119,55 @@ string_to_array:
 	
 get_length:
 	lb $t0, newline
+	or $v0, $zero, $zero
+len_loop:
+	lw $t1, 0($a0)
+	beq $t1, $t0, len_done
+	addi $v0, $v0, 1
+	addi $a0, $a0, 4
+	j len_loop
+len_done:
 	jr $ra
 	
 is_pali_loop:
+	or $t1, $a1, $a1
+	sll $t0, $a1, 2
+	or $t2, $a0, $a0
+	add $t3, $a0, $t0
+	addi $t3, $t3, -4
+	ori $v0, $zero, 1
+pali_loop:
+	beq $t1, $zero, end_pali_loop
+	addi $t1, $t1, -1
+	lw $t4, 0($t2)
+	lw $t5, 0($t3)
+	beq $t4, $t5, cont_pali_loop
+	or $v0, $zero, $zero
+	j end_pali_loop
+cont_pali_loop:
+	addi $t2, $t2, 4
+	addi $t3, $t3, -4
+	j pali_loop
+end_pali_loop:
 	jr $ra
 	
 is_pali_recursive:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	ori $v0, $zero, 1
+	slti $t0, $a1, 2
+	bne $t0, $zero, pali_rec_done
+	sll $t0, $a1, 2
+	addi $t0, $t0, -4
+	add $t0, $t0, $a0
+	lw $t1, 0($t0)
+	lw $t2, 0($a0)
+	or $v0, $zero, $zero
+	bne $t1, $t2, pali_rec_done
+	addi $a1, $a1, -2
+	addi $a0, $a0, 4
+	jal is_pali_recursive
+pali_rec_done:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
 	jr $ra
